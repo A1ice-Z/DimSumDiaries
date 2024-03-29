88265cadaba0d194023a36bb9f6c58a79c238a6a
Alice <alicezheng888@gmail.com>
2024-03-29 14:34:31 +0100

    git init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center">
+        <p> Stupid Simon </p>
+      </div>
+    </>
+  )
+}
+
+export default App;
