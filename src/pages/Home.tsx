3b92672bceba32283af25cf1de7eaf70306df400
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-03-29 17:35:53 +0200

    Feat(#2): Update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import Navbar from "../components/ Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
 const Home = () => {
     return (
         <>
-            <p className="text-black">Stupid Alice</p>
+            <Navbar />
+            <main className="h-[650px] w-full font-palatino">
+                <div className="h-full items-center flex w-full justify-center">
+                    <p className="text-[58px] underline">Stupid Alice</p>
+                </div>
+            </main>
+            <Footer />
         </>
     );
 };
