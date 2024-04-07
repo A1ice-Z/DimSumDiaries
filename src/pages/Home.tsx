e28a065aac576c71689eec0fe018f3f1f440b555
Alice <alicezheng888@gmail.com>
2024-04-07 22:34:16 +0200

    feat(#3): added cats

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
             <Navbar />
             <main className="h-[650px] w-full font-palatino">
                 <div className="h-full items-center flex w-full justify-center">
-                    <p className="text-[58px] underline">Stupid Alice</p>
+                    <p className="text-[58px] underline"></p>
                     <FoodCard imageSrc={"https://redhousespice.com/wp-content/uploads/2020/06/Jian-Bing-Chinese-crepe-10-1367x2048.jpg"} altText={"Good Food"} caption={"Jian Bing - 煎饼"}            />
                 </div>
             </main>
