85e1f777441a5cc80ba12a4eb5738ed72cc0eb8a
Alice <alicezheng888@gmail.com>
2024-04-07 22:34:16 +0200

    feat(#3): added cats

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,47 +13,8 @@ const Home = () => {
             <Navbar />
             <main className="h-[650px] w-full font-palatino">
                 <div className="h-full items-center flex w-full justify-center">
-                    <p className="text-[58px] underline">Stupid Alice</p>
-                    <FoodCard
-                        imageSrc={
-                            "https://redhousespice.com/wp-content/uploads/2020/06/Jian-Bing-Chinese-crepe-10-1367x2048.jpg"
-                        }
-                        altText={"Good Food"}
-                        caption={"Jian Bing - 煎饼"}
-                    />
-                </div>
-                <div className="py-[20px] px-[5%] font-palatino text-[30px]">
-                    <div className="py-[15px]">
-                        <div className="flex gap-2">
-                            <p>My Favorites</p>
-                            <div>
-                                <FiHeart className="h-full items-end" />
-                            </div>
-                        </div>
-                        <div></div>
-                    </div>
-                    <div className="py-[15px]">
-                        <div className="flex gap-2">
-                            <p>This Weeks Recepies</p>
-                            <div>
-                                <LuCalendarHeart className="h-full items-end" />
-                            </div>
-                        </div>
-                        <div></div>
-                    </div>
-                    <div className="py-[15px]">
-                        <div className="flex gap-2">
-                            <p>All recepies</p>
-                            <div>
-                                <PiBowlFoodFill className="h-full items-end" />
-                            </div>
-                        </div>
-                        <div className="flex items-start justify-center gap-6">
-                            {recipes.map((card: foodCardType) => (
-                                <FoodCard {...card} />
-                            ))}
-                        </div>
-                    </div>
+                    <p className="text-[58px] underline"></p>
+                    <FoodCard imageSrc={"https://redhousespice.com/wp-content/uploads/2020/06/Jian-Bing-Chinese-crepe-10-1367x2048.jpg"} altText={"Good Food"} caption={"Jian Bing - 煎饼"}            />
                 </div>
             </main>
             <Footer />
