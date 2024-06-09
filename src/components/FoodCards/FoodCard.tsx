b81cbfe86eb4c4d55fc79a2fcdc44078c6dd6d2f
Alice <alicezheng888@gmail.com>
2024-06-09 13:31:02 +0200

    feat(#4): Clicking on food card will open the correct recipe

--- src/components/FoodCards/FoodCard.tsx
+++ src/components/FoodCards/FoodCard.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import foodCardType from "../../types/FoodCardTypes";
 import { FiHeart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 interface FoodCardProps {
     id: string;
@@ -45,17 +46,17 @@ const FoodCard = ({ id, imageSrc, altText, caption }: FoodCardProps) => {
         }
     }
 
-    useEffect(() => {
-        console.log(favorites)
-    }, [favorites])
     return (
-        <div className="w-[150px] h-[150px] rounded-2xl overflow-hidden shadow-lg my-2"
-            onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <img className="object-cover w-full h-[105px]" src={imageSrc} alt={altText} />
-            <div className="px-6 py-4 bg-cream text-center w-full h-[45px] flex items-center justify-center">
-                <div className=" font-bold text-sm mb-2 mt-2">{isHovered ? <FiHeart size={22} color={isFavorite ? 'red' : 'black'} onClick={handleFavorites} className="cursor-pointer" /> : caption}</div>
+
+        <Link to={"/recipe/" + id}>
+            <div className="w-[150px] h-[150px] rounded-2xl overflow-hidden shadow-lg my-2"
+                onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                <img className="object-cover w-full h-[105px]" src={imageSrc} alt={altText} />
+                <div className="px-6 py-4 bg-cream text-center w-full h-[45px] flex items-center justify-center">
+                    <div className=" font-bold text-sm mb-2 mt-2">{isHovered ? <FiHeart size={22} color={isFavorite ? 'red' : 'black'} onClick={handleFavorites} className="cursor-pointer" /> : caption}</div>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
