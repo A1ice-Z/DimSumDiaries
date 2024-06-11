53c3e06b44f8b7b0ed15edfd474b97fe1ad4c4b2
Alice <alicezheng888@gmail.com>
2024-06-11 14:20:53 +0200

    feat(#14): add pointers to scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import { PiBowlFoodFill } from "react-icons/pi";
-import { FiHeart } from "react-icons/fi";
+import { PiBowlFoodFill, } from "react-icons/pi";
+import { FiHeart, FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { LuCalendarHeart } from "react-icons/lu";
 import recipes from "../data/FoodCards";
 import FoodCardList from "../components/FoodCards/FoodCardList";
@@ -9,6 +9,9 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [favorites, setFavorites] = useState<String[]>([]);
+    const [leftPosition, setLeftPosition] = useState<boolean>(true)
+    const [rightPosition, setRightPosition] = useState<boolean>(true)
+
 
     useEffect(() => {
         const currentFavorites = sessionStorage.getItem("favorites");
@@ -20,10 +23,37 @@ const Home = () => {
         }
     }, []);
 
+    const handleScroll = () => {
+        const element = document.getElementById("scrollable")!
+        if (element.scrollLeft == 0) {
+            setLeftPosition(true)
+        }
+        else {
+            setLeftPosition(false)
+        }
+
+        if (element.scrollWidth - element.scrollLeft === element.clientWidth) {
+            setRightPosition(true)
+        }
+        else {
+            setRightPosition(false)
+        }
+    }
+
+
+    const scroll = (scrollOffset: any) => {
+        if (document.getElementById("scrollable") !== null) {
+            const element = document.getElementById("scrollable")!
+            element.scrollBy({ left: scrollOffset, behavior: "smooth" })
+        }
+
+    };
+
+
     return (
         <>
             <Navbar />
-            <main className="h-fit w-full font-palatino">
+            <main className="h-fit w-full font-palatino overflow-hidden">
                 <div className="bg-cream h-[330px] flex items-center">
                     <div className="bg-cream-light py-[10px] w-full h-[270px]">
                         <div className="text-[40px] h-full w-full flex items-end justify-start px-[5%] gap-[52%]">
@@ -45,7 +75,7 @@ const Home = () => {
                 </div>
                 <div className="py-[20px] font-palatino text-[30px]">
                     <div className="py-[15px] ">
-                        <div className="flex gap-2 pl-[5%]">
+                        <div className="flex gap-2 pl-[5%] pb-4">
                             <p>My Favorites</p>
                             <div>
                                 <FiHeart className="h-full items-end" />
@@ -61,7 +91,7 @@ const Home = () => {
                         </div>
                     </div>
                     <div className="py-[15px]">
-                        <div className="flex gap-2 pl-[5%]">
+                        <div className="flex gap-2 pl-[5%] pb-4">
                             <p>This Weeks Recepies</p>
                             <div>
                                 <LuCalendarHeart className="h-full items-end" />
@@ -70,17 +100,27 @@ const Home = () => {
                         <div></div>
                     </div>
                     <div className="py-[15px]">
-                        <div className="flex gap-2 pl-[5%]">
+                        <div className="flex gap-2 pl-[5%] pb-4">
                             <p>All recepies</p>
                             <div>
                                 <PiBowlFoodFill className="h-full items-end" />
                             </div>
                         </div>
-                        <div className="relative h-fit w-full overflow-hidden overflow-x-scroll scrollbar-hide">
-                            <div className="inline-block relative left-[5%]">
-                                <FoodCardList recipes={recipes} />
+                        <div className="flex items-center group">
+                            {!leftPosition && <div className="absolute px-2 py-2 left-[1%] z-[5] bg-cream group-hover:opacity-100 opacity-0 transition-all rounded-md text-[30px] shadow-md hover:bg-cream-light cursor-pointer" onClick={() => scroll(-500)}>
+                                <FiChevronLeft />
+                            </div>}
+                            {!rightPosition && <div className="absolute px-2 py-2 right-[1%] z-[5] bg-cream group-hover:opacity-100 opacity-0 transition-all rounded-md text-[30px] shadow-md hover:bg-cream-light cursor-pointer" onClick={() => scroll(500)}>
+                                <FiChevronRight />
+                            </div>}
+                            <div id="scrollable" className="relative h-fit w-full overflow-hidden overflow-x-scroll scrollbar-hide" onScroll={() => handleScroll()}>
+
+                                <div className="inline-block relative left-[5%]">
+                                    <FoodCardList recipes={recipes} />
+                                </div>
                             </div>
                         </div>
+
 
                     </div>
                 </div>
